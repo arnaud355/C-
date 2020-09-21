4eca63a409806c76e9773fc4134b0d2ae9357cd7
arnaud355 <arnaud.baleh@avanade.com>
2020-09-21 15:57:55 +0200

    add listeChainee

---
+++ ListeChainee/ListeChainee/ListeGenerique.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListeChainee
+{
+    public class ListeGenerique<T>
+    {
+
+        private int capacite;
+        private int nbElements;
+        private T[] tableau;
+
+        public int suivant { get; }
+        public int precedent { get; }
+
+        private T premierElement;
+        private T dernierElement;
+
+        public ListeGenerique()
+        {
+            capacite = 10;
+            nbElements = 0;
+            tableau = new T[capacite];
+
+        }
+        public int GetnbElements()
+        {
+            return nbElements;
+        }
+        
+        public T GetSuivant(int indice)
+        {
+            int indiceSuivant = indice + 1;
+        
+            if (indiceSuivant >= nbElements)
+            {
+                Console.WriteLine("Pas d'element suivant");
+                return default(T);
+            }
+            else
+            {
+                return tableau[indiceSuivant];
+            }
+                       
+        }
+
+        public T GetPrecedent(int indice)
+        {
+            int indicePrecedent = indice - 1;
+
+            if (indicePrecedent < 0)
+            {
+                Console.WriteLine("Pas d'element precedent");
+                return default(T);
+            }
+            else
+            {
+                return tableau[indicePrecedent];
+            }
+                    
+        }
+        public T GetpremierElement()
+        {
+            premierElement = tableau[0];
+            return premierElement;
+        }
+        public T GetdernierElement()
+        {
+            dernierElement = tableau[nbElements - 1];
+            return dernierElement;
+        }
+       
+        public void Ajouter(T element)
+        {
+            if (nbElements >= capacite)
+            {
+                capacite *= 2;
+                T[] copieTableau = new T[capacite];
+                for (int i = 0; i < tableau.Length; i++)
+                {
+                    copieTableau[i] = tableau[i];
+                }
+                tableau = copieTableau;
+            }
+            tableau[nbElements] = element;
+            nbElements++;
+        }
+
+        public T ObtenirElement(int indice)
+        {
+            if (indice < 0 || indice >= nbElements)
+            {
+                Console.WriteLine("L'indice n'est pas bon");
+                return default(T);
+            }
+            return tableau[indice];
+        }
+
+        public void AjoutElementFin(T ele)
+        {
+            nbElements++;
+            T[] copieTableau = new T[nbElements];
+            for (int i = 0; i < copieTableau.Length - 1; i++)
+            {
+                copieTableau[i] = tableau[i];
+            }
+            copieTableau[nbElements - 1] = ele;
+            Console.WriteLine("ele:{0}", ele);
+            tableau = copieTableau;
+
+        }
+        public void GetAllElements()
+        {
+            for (int i = 0; i < this.GetnbElements(); i++)
+            {
+                Console.WriteLine(tableau[i]);
+            }
+        }
+
+        public void InsertEleToIndice(T ele, int indice)
+         {
+             T temp, suivant;
+        
+
+             if (indice < 0 || indice >= nbElements)
+             {
+                 Console.WriteLine("L'indice est inférieur à zéro ou sup au nombre d'elements");
+             }
+
+             if (indice > 0)
+             {
+                 indice -= 1;
+             }
+
+             nbElements++;
+             //On copie le tableau, en lui donnant un indice de plus, qui sera à 0 par defaut
+             T[] copieTableau = new T[nbElements];
+             for (int j = 0; j < nbElements - 1; j++)
+             {
+                 copieTableau[j] = this.tableau[j];
+             }
+
+            for (int i = indice;i < nbElements; i++)
+             {
+                temp = ele;
+                suivant = copieTableau[i];
+                copieTableau[i] = temp;              
+                ele = suivant;
+             }
+
+            this.tableau = copieTableau;
+           
+         }
+    }
+}
---
+++ ListeChainee/ListeChainee/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace ListeChainee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListeGenerique<int> maListe = new ListeGenerique<int>();
+            maListe.Ajouter(24);
+            maListe.Ajouter(25);
+            maListe.Ajouter(21);
+
+            Console.WriteLine(maListe.ObtenirElement(0));
+            Console.WriteLine(maListe.ObtenirElement(1));
+            Console.WriteLine(maListe.ObtenirElement(2));
+
+            Console.WriteLine("Element precedent: {0}", maListe.GetPrecedent(0));
+            Console.WriteLine("Element precedent: {0}", maListe.GetPrecedent(2));
+            Console.WriteLine("Element suivant: {0}", maListe.GetSuivant(1));
+            Console.WriteLine("Element suivant: {0}", maListe.GetSuivant(257));
+
+
+            maListe.AjoutElementFin(5);
+            maListe.AjoutElementFin(10);
+            maListe.AjoutElementFin(4);
+
+            Console.WriteLine("liste d'elements:");
+            for (int i = 0; i < maListe.GetnbElements(); i++)
+            {
+                maListe.GetSuivant(i);
+            }
+            for (int i = maListe.GetnbElements() - 1; i >= 0; i--)
+            {
+                maListe.GetPrecedent(i);
+            }
+            Console.WriteLine("premier element: {0}", maListe.GetpremierElement());
+            Console.WriteLine("dernier element: {0}", maListe.GetdernierElement());
+
+            maListe.InsertEleToIndice(99, 0);
+            maListe.InsertEleToIndice(33, 2);
+            maListe.InsertEleToIndice(30, 2);
+
+            maListe.GetAllElements();
+
+            /*
+            for (int i = 0; i < 30; i++)
+            {
+                maListe.Ajouter(i);
+            } */
+        }
+    }
+}
+
